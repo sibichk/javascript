f8cd823dec1804776bc4b7607e2b2cb03e274de3
sibi <sibi27@yahoo.com>
2014-07-17 21:13:58 -0700

    new lessons :)

--- node/hello.js
+++ node/hello.js
@@ -1,7 +1,6 @@
 var http= require ('http');
-http.createServer=(function(request,response){
-	response.writeHead(200);
-	response.write("you're awesome");
-	response.end();
-}).listen(8080);
-console.log('running bitches');
+var server = http.createServer(function(request,response){
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end("Hello World");
+});
+server.listen(8000);
